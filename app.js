6773df68fb14cfb8a2e6b288203b2bc3df6c3a37
rahulpasupuleti2024 <rahulpasupuleti2@gmail.com>
2023-07-09 12:25:31 +0530

    Adding worklist

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food"];
+let items = ["Buy Food", "Cook Food"]; 
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,18 +25,34 @@ app.get("/", function(req, res){
     
 
     
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
      let item = req.body.newItem;
 
+     if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+     }else{
+
      items.push(item);
 
      res.redirect("/");
+     }
      
-})
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+// app.post("/work", function(req,res){
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
 
 
 
