2afd93605b179d1c9c1405994f844241d29f102c
rahulpasupuleti2024 <rahulpasupuleti2@gmail.com>
2023-07-09 16:30:03 +0530

    exports module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
@@ -13,16 +15,9 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-    let today = new Date();
-    
-    let options ={
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
-    
-    
+
+    let day = date.getDate();
+
 
     
     res.render("list", {listTitle: day, newListItems: items});
